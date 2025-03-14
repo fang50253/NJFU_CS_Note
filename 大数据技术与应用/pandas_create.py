af87529081967c924fdaee78792798cc6c2100a1
fang50253 <fzy1139950253@gmail.com>
2025-03-14 11:14:18 +0800

    更新

--- 大数据技术与应用/pandas_create.py
+++ 大数据技术与应用/pandas_create.py
@@ -1,12 +1,20 @@
 import numpy as np
 import pandas as pd
-
 df = pd.DataFrame(
     data=np.arange(12).reshape((4, 3)),
     index=list("ABCD"),
     columns=["aa", "bb", "cc"]
 )
-
 print(df)
 
-print(df.describe())
+print(df.describe()) # 数据摘要
+
+print(df.T) # 输出转置
+
+df["dd"]=12 # 增加一列dd，设置为12
+print(df)
+
+df.drop("A",inplace=True) # 如果不设置inplace，返回值不会被接收
+print(df)
+
+df.info()
