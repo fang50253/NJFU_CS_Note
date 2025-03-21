d68677467d29d0a36800d1829d44deb52e008969
fang50253 <136136871+fang50253@users.noreply.github.com>
2025-03-21 10:56:33 +0800

    Add files via upload

---
+++ 大数据技术与应用/第五周上机作业/pythonProject1/unit4.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+health_care_dataset_stroke=pd.read_csv("../healthcare-dataset-stroke-data.csv")
+health_care_dataset_age_abs=pd.read_csv("../stroke-data.csv")
+
+print(f"health_care_dataset_stroke 数据量: {len(health_care_dataset_stroke)} 行")
+print(f"health_care_dataset_age_abs 数据量: {len(health_care_dataset_age_abs)} 行")
+
+merged_data = pd.merge(
+    health_care_dataset_stroke,
+    health_care_dataset_age_abs,
+    on="avg_glucose_level",
+    how="outer",
+    indicator=True
+)
+
+print(merged_data)
+
